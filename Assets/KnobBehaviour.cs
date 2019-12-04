95fe20376af981f6e8591614075a630b5456fa60
Jason Ponce <jcponce11@gmail.com>
2019-12-03 19:00:46 -0600

    Working Knob and Flame

--- Assets/KnobBehaviour.cs
+++ Assets/KnobBehaviour.cs
@@ -1,27 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK.GrabAttachMechanics;
+using VRTK.SecondaryControllerGrabActions;
+using VRTK.Controllables.ArtificialBased;
 
 public class KnobBehaviour : MonoBehaviour
 {
-    public Animator anim;
-    bool isBurnerActive;
+    public bool isBurnerActive;
+    [SerializeField] VRTK_ArtificialRotator BunsenRotater;
+    public ParticleSystem particleSystemReference;
+    [SerializeField] GameObject HotArea;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         isBurnerActive = false;
-        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ActivateFlameParticleSystem();
+        ActivateFlameHeat();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void ActivateFlameHeat()
     {
-        isBurnerActive = !isBurnerActive;
-        anim.Play("BunsenButtonPress");
+        if (isBurnerActive)
+            HotArea.SetActive(true);
+        else
+            HotArea.SetActive(false);
+    }
+
+    void ActivateFlameParticleSystem()
+    {
+        if (BunsenRotater.GetNormalizedValue() > .75f)
+        {
+            isBurnerActive = true;
+            particleSystemReference.Play();
+        }
+        else if (BunsenRotater.GetNormalizedValue() < .75f)
+        {
+            isBurnerActive = false;
+            particleSystemReference.Stop();
+        }
     }
 }
